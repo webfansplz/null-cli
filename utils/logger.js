f1d78d013cc16ac61c6c08aedc72695f664f8dd0
webfansplz <>
2019-11-28 18:41:12 +0800

    fix: rewrite test case

--- utils/logger.js
+++ utils/logger.js
@@ -1,13 +1,13 @@
-const readline = require('readline');
-const chalk = require('chalk');
+const readline = require('readline')
+const chalk = require('chalk')
 
 exports.clearConsole = () => {
   if (process.stdout.isTTY) {
-    const blank = '\n'.repeat(process.stdout.rows);
-    console.log(blank);
-    readline.cursorTo(process.stdout, 0, 0);
-    readline.clearScreenDown(process.stdout);
-    const { version } = require('../package.json');
-    console.log(chalk.bold.blue(`🌟️ Mamba CLI v${version}`));
+    const blank = '\n'.repeat(process.stdout.rows)
+    console.log(blank)
+    readline.cursorTo(process.stdout, 0, 0)
+    readline.clearScreenDown(process.stdout)
+    const { version } = require('../package.json')
+    console.log(chalk.bold.blue(`🌟️ Null CLI v${version}`))
   }
-};
+}
